a18b2a2f972bc9c2508b7516b6285da6d34a1023
Mark P. Lindhout <mark@webambacht.nl>
2014-04-14 13:50:09 +0200

    Initial import of the new basic meatmap HTML, JS and CSS files. Based on HTML Boilerplate, Modernizr, Leaflet and jQuery 2.1.0

---
+++ js/main.js
@@ -0,0 +1,107 @@
+var start_coordinates = new L.LatLng(52.501045, 13.442569);
+
+var map = L.map('map', {
+    center: start_coordinates,
+    zoom: 15,
+});
+
+var locations_burger = [];
+var locations_bbq    = [];
+
+function mm_load () {
+
+	for (var j = 0; j < mm_markers.length; j++) {
+
+		var location = false;
+		var popup_text = "";
+		var lat = false;
+		var len = false;
+		var type = false;
+
+		if ( mm_markers[j].hasOwnProperty('name') ) {
+			popup_text += '<h2>' + mm_markers[j]['name'] + '</h2>\n';
+		}
+
+		if ( mm_markers[j].hasOwnProperty('comment') ) {
+			popup_text += '<p>' + mm_markers[j]['comment'] + '</p>\n';
+		}
+
+		if ( mm_markers[j].hasOwnProperty('phone') ) {
+			popup_text += '<p><strong>' + mm_markers[j]['phone'] + '</strong></p>\n';
+		}
+
+		if ( mm_markers[j].hasOwnProperty('email') ) {
+			popup_text += '<p>' + mm_markers[j]['email'] + '</p>\n';
+		}
+
+		if ( mm_markers[j].hasOwnProperty('url') ) {
+			popup_text += '<p><a href="' + mm_markers[j]['url'] + '" rel="nofollow" target="_blank">' + mm_markers[j]['url'] + '</a></p>\n';
+		}
+
+		if ( mm_markers[j].hasOwnProperty('lat') ) {
+			lat = mm_markers[j]['lat'];
+		}
+
+		if ( mm_markers[j].hasOwnProperty('lng') ) {
+			lng = mm_markers[j]['lng'];
+		}
+
+		if ( mm_markers[j].hasOwnProperty('type') ) {
+			type = mm_markers[j]['type'];
+			icont = new MMIcon({iconUrl: 'img/mm_marker_' + type + '.png'});
+
+			location = L.marker([lat, lng], {icon: icont});
+
+			if (type === 'burger' ) {
+				locations_burger.push(location);
+			}
+			else if (type === 'bbq') {
+				locations_bbq.push(location);
+			}
+		}
+		
+		location.bindPopup(popup_text);
+	}
+
+	locations_burger = L.layerGroup(locations_burger);
+	locations_bbq    = L.layerGroup(locations_bbq);
+
+}
+
+// var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+// 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+var tiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/marklindhout.hpk7ih6p/{z}/{x}/{y}.png', {
+    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+}).addTo(map);
+
+var MMIcon = L.Icon.extend({
+    options: {
+		// shadowUrl: 'leaf-shadow.png',
+		iconSize:     [56, 79],
+		// shadowSize:   [50, 64],
+		iconAnchor:   [28, 79],
+		// shadowAnchor: [4, 62],
+		popupAnchor:  [0, -79]
+    }
+});
+
+$(document).ready(function() {
+
+	mm_load();
+
+	locations_burger.addTo(map);
+	locations_bbq.addTo(map);
+	L.control.layers(
+		null,
+		{
+			"<img src='img/mm_marker_bbq.png' /> BBQ": locations_bbq,
+			"<img src='img/mm_marker_burger.png' /> Burgers": locations_burger
+		},
+		{
+			collapsed: false
+		})
+	.addTo(map);
+
+});
---
+++ mm_markers_json.js
@@ -0,0 +1,461 @@
+var mm_markers = [
+    {
+        "name": "Dirty South",
+        "address": "Krossener Strasse 18, 10245 Friedrichshain",
+        "lat": "52.510509",
+        "lng": "13.457817",
+        "type": "bbq",
+        "phone": "030 29360555",
+        "email": "info@dirtysouthberlin.de",
+        "url": "http://www.dirtysouthberlin.de"
+    },
+    {
+        "name": "Brooklyn Beef Club",
+        "address": "Koepenicker Strasse 92, 10179 Mitte",
+        "lat": "52.511719",
+        "lng": "13.413596",
+        "type": "bbq",
+        "phone": "030 20215820",
+        "email": "table@brooklynbeefclub.com",
+        "url": "http://www.brooklynbeefclub.com"
+    },
+    {
+        "name": "The Bird",
+        "address": "Am Falkplatz 5, 10437 Prenzlauer Berg",
+        "lat": "52.546726",
+        "lng": "13.405719",
+        "type": "bbq",
+        "phone": "030 51053283",
+        "email": "thebirdreservations@gmail.com",
+        "url": "http://www.thebirdinberlin.com"
+    },
+    {
+        "name": "Chicago Williams BBQ",
+        "address": "Hannoversche Str. 2, 10115 Mitte",
+        "lat": "52.527126",
+        "lng": "13.385616",
+        "type": "bbq",
+        "phone": "030 28042422",
+        "email": "info@chicagowilliamsbbq.com",
+        "url": "http://www.chicagowilliamsbbq.com",
+        "comment": "Eigentlich kann man bei Chicago Williams alles essen was auf der Karte (bzw. der Tafel) steht. Fuer den Einstieg kann ich aber vor allem das \"Porn Rib\" und das \"Pulled Pork\" empfehlen!"
+    },
+    {
+        "name": "Big Stuff Smoked BBQ",
+        "address": "Eisenbahnstrasse 42, 10997 Kreuzberg",
+        "lat": "52.501984",
+        "lng": "13.431982",
+        "type": "bbq",
+        "phone": "0163 6290413",
+        "email": "hallo@bigstuff.de",
+        "url": "http://www.bigstuff.de"
+    },
+    {
+        "name": "Burgermeister",
+        "address": "Oberbaumstrasse 8, 10997 Berlin",
+        "lat": "52.501179",
+        "lng": "13.442510",
+        "type": "burger",
+        "phone": "030 26348764",
+        "email": "contact@burger-meister.de",
+        "url": "http://www.burger-meister.de"
+    },
+    {
+        "name": "Burgeramt",
+        "address": " Krossener Strasse 22, 10245 Berlin",
+        "lat": "52.510242",
+        "lng": "13.459508",
+        "type": "burger",
+        "phone": " 030 667 634 53",
+        "email": "burgeramt@hotmail.de",
+        "url": "http://www.burgeramt.com"
+    },
+    {
+        "name": "Ketchup und Majo",
+        "address": "Oranienstrasse 37, 10999 Berlin",
+        "lat": "52.501842",
+        "lng": "13.417673",
+        "type": "burger",
+        "phone": "030 62901400",
+        "email": " info@ketchup-majo.de",
+        "url": "http://www.ketchup-majo.de"
+    },
+    {
+        "name": "Schiller Burger 2",
+        "address": "Schoenleinstr. 34, 10967 Berlin",
+        "lat": "52.492664",
+        "lng": "13.421691",
+        "type": "burger",
+        "phone": "N/A",
+        "email": "N/A",
+        "url": "N/A"
+    },
+    {
+        "name": "Burgers Berlin",
+        "address": "Sonntagstr. 3, Ostkreuz Friedrichshain",
+        "lat": "52.504734",
+        "lng": "13.467733",
+        "type": "burger",
+        "phone": "030 29364069",
+        "email": "N/A",
+        "url": "http://www.burgers-berlin.com"
+    },
+    {
+        "name": "Daisy's Diner",
+        "address": "An der Autobahn 2 in Oyten bei Bremen",
+        "lat": "53.053715",
+        "lng": "9.030742",
+        "type": "burger",
+        "phone": "04207 4884",
+        "email": "info@daisys-diner.de",
+        "url": "http://www.daisys-diner.de"
+    },
+    {
+        "name": "Zsa Zsa Burger",
+        "address": "Motzstrasse 28, 10777 Schoeneberg",
+        "lat": "52.497856",
+        "lng": "13.347871",
+        "type": "burger",
+        "phone": "030 2191347",
+        "email": "info@zsazsaburger.de",
+        "url": "http://www.zsazsaburger.de"
+    },
+    {
+        "name": "Burger House",
+        "address": "Rablstrasse 37, 81667 Muenchen",
+        "lat": "48.127293",
+        "lng": "11.592740",
+        "type": "burger",
+        "phone": "089 88903550",
+        "email": "info@theburgerhouse.com",
+        "url": "http://www.theburgerhouse.com "
+    },
+    {
+        "name": "The Grill",
+        "address": "In der Vahr 64, 28329 Bremen",
+        "lat": "53.081955",
+        "lng": "8.876429",
+        "type": "bbq",
+        "phone": "0421 87825640",
+        "email": "info@the-grill-bremen.de",
+        "url": "http://www.the-grill-bremen.de"
+    },
+    {
+        "name": "OX U.S. Steakhouse",
+        "address": "Gueldenstrasse 7, 38100 Braunschweig",
+        "lat": "52.260353",
+        "lng": "10.515190",
+        "type": "bbq",
+        "phone": "0531 243900",
+        "email": "info@oxsteakhouse.com",
+        "url": "http://www.oxsteakhouse.com"
+    },
+    {
+        "name": "Burger de Ville",
+        "address": "Hardenbergstrasse 29a, 10623 Berlin",
+        "lat": "52.505901",
+        "lng": "13.333966",
+        "type": "burger",
+        "phone": "030 68073949",
+        "email": "N/A",
+        "url": "N/A",
+        "comment": "Die Auswahl hier mag zwar nicht sehr groߠsein, aber dafuer ist das, was angeboten wird sehr zu empfehlen. Burger vom feinsten \"Black Angus Rind\" und echtem Cheddar, auf geroestetem Burgerbrot. Hier kommt der Geschmack noch von den Hauptzutaten!"
+    },
+    {
+        "name": "Mels's Diner",
+        "address": "Hildesheimer Strasse 82, 30880 Laatzen",
+        "lat": "52.313969",
+        "lng": "9.792624",
+        "type": "burger",
+        "phone": "0511 862310",
+        "email": "info@mels-diner.de",
+        "url": "http://www.mels-diner.de"
+    },
+    {
+        "name": "Road House",
+        "address": "Bielefelder Strasse 141, 33378 Rheda-Wiedenbrueck",
+        "lat": "51.848824",
+        "lng": "8.322779",
+        "type": "bbq",
+        "phone": "05242 54699",
+        "email": "info@roadhouse-germany.de",
+        "url": "http://www.roadhouse-germany.de"
+    },
+    {
+        "name": "Die fette Kuh",
+        "address": "Bonner Strasse 43,  50677 Koeln",
+        "lat": "50.918163",
+        "lng": "6.960608",
+        "type": "burger",
+        "phone": "0221 37627775",
+        "email": "info@die-fette-kuh.de",
+        "url": "http://www.facebook.com/DiefetteKuh/"
+    },
+    {
+        "name": "Beef Brothers",
+        "address": "Aachener Str. 12, 50674 Koeln",
+        "lat": "50.936600",
+        "lng": "6.937054",
+        "type": "burger",
+        "phone": "0221 29834736",
+        "email": "info@beef-brothers.de",
+        "url": "http://www.beef-brothers.de"
+    },
+    {
+        "name": "Marienburger",
+        "address": "Marienburger Strasse 47, 10405 Prenzlauer Berg",
+        "lat": "52.535328",
+        "lng": "13.423320",
+        "type": "burger",
+        "phone": "030 30340515",
+        "email": "info@marienburger-berlin.de",
+        "url": "http://www.marienburger-berlin.de "
+    },
+    {
+        "name": "Burger Bar",
+        "address": "Kolksteeg 2, 1012 Amsterdam",
+        "lat": "52.376171",
+        "lng": "4.895530",
+        "type": "burger",
+        "phone": "020 6249049",
+        "email": "info@burger-bar.nl",
+        "url": "http://www.burger-bar.nl",
+        "comment": "Best Burger in Europe<br/>-Mark"
+    },
+    {
+        "name": "Burger Bar",
+        "address": "Reguliersbreestraat 9 BG, 1017 CL Amsterdam",
+        "lat": "52.366859",
+        "lng": "4.894110",
+        "type": "burger",
+        "phone": " 020 3305968",
+        "email": "info@burger-bar.nl",
+        "url": "http://www.burger-bar.nl",
+        "comment": "Best Burger in Europe<br/>-Mark"
+    },
+    {
+        "name": "Burger Bar",
+        "address": "Warmoesstraat 21, 1012 HT Amsterdam",
+        "lat": "52.375820",
+        "lng": "4.899072",
+        "type": "burger",
+        "phone": "020 7740300",
+        "email": "info@burger-bar.nl",
+        "url": "http://www.burger-bar.nl",
+        "comment": "Best Burger in Europe<br/>-Mark"
+    },
+    {
+        "name": "Berlinburger International",
+        "address": "Pannierstrasse 5, 12047 Berlin",
+        "lat": "52.486519",
+        "lng": "13.430934",
+        "type": "burger",
+        "phone": "0178 5407409",
+        "email": " info@berlinburgerinternational.com",
+        "url": "http://www.berlinburgerinternational.com"
+    },
+    {
+        "name": "Room 77",
+        "address": "Graefestrasse 77, 10967 Berlin",
+        "lat": "52.493019",
+        "lng": "13.417993",
+        "type": "burger",
+        "phone": "030 31102260",
+        "email": "info@room77.de",
+        "url": "http://www.room77.de",
+        "comment": "Leckere amerikanische Burritos, Nachos und Quesadillas und klassische Burger im texanischen Flair."
+    },
+    {
+        "name": "to beef or not to beef",
+        "address": "Akazienstrasse 3, 10823 Schoeneberg",
+        "lat": "52.486595",
+        "lng": "13.355669",
+        "type": "bbq",
+        "phone": "030  54599047",
+        "url": "http://www.tobeefornottobeef.de",
+        "comment": "Leckeres Lammkarree, tolle Burger und ziemlich grosse Mengen von sehr gutem Fleisch, hervorragend zubereitet. Lustiger weise wird das Steak nach Gewicht abgerechnet.<br/>Sehr zu empfehlen!<br/><br/>- Alex B."
+    },
+    {
+        "name": "M. C. Mueller",
+        "address": "Fraunhoferstr. 2, 80469 Muenchen",
+        "lat": "48.131126",
+        "lng": "11.571477",
+        "type": "burger",
+        "phone": "089 18910039",
+        "email": "nick@mcmueller.org",
+        "url": "http://www.mcmueller.org",
+        "comment": "Fleischvielfalt (z.B. Auswahl Kobe-Rind), interessante Kombinationen, selbstgemachte Pommes, wundervolle Saucen-Kreationen.<br/>- Koray"
+    },
+    {
+        "name": "StarChief Diner",
+        "address": "Grimbergstr. 14, 45889 Gelsenkirchen",
+        "lat": "51.540512",
+        "lng": "7.114301",
+        "type": "burger",
+        "phone": "0209 8008250",
+        "email": "ge@starchiefdiner.com",
+        "url": "http://www.starchiefdiner.com",
+        "comment": "Am Wochenenden, sollte man auf jeden Fall reservieren. Das Auswahl ist vielfaeltig und wird ergaenzt von Aktionen und Angeboten. Im Sommer gibt es eine nette Aussengastronomie mit Bar."
+    },
+    {
+        "name": "DerFreiBurger",
+        "address": "Schiffstrasse 16, 79098 Freiburg im Breisgau",
+        "lat": "47.997162",
+        "lng": "7.849907",
+        "type": "burger",
+        "phone": "Nicht vorhanden",
+        "email": "info@derfreiburger.de",
+        "url": "http://www.derfreiburger.de",
+        "comment": "Der beste Burgerladen in Freiburg."
+    },
+    {
+        "name": "Burger Ring",
+        "address": "Neue Bahnhofstr. 7A, 10245 Berlin-Friedrichshain, Berlin, Germany",
+        "lat": "52.505630",
+        "lng": "13.470034",
+        "type": "burger",
+        "phone": "030 20050905",
+        "email": "burgerringberlin@gmail.com",
+        "url": "http://www.burgerring.de"
+    },
+    {
+        "name": "Zum goldenen Kalb",
+        "address": "Utzschneiderstrasse 1, 80469 Muenchen",
+        "lat": "48.134331",
+        "lng": "11.575333",
+        "type": "bbq",
+        "phone": "089.235 422 90",
+        "email": "info@zum-goldenen-kalb.de",
+        "url": "http//www.zum-goldenen-kalb.de"
+    },
+    {
+        "name": "Alte Metzgerei",
+        "address": "Bergisch Gladbacher Strasse 974, 51069 Koeln",
+        "lat": "50.977821",
+        "lng": "7.068377",
+        "type": "burger",
+        "url": "https://www.facebook.com/AlteMetzgerei"
+    },
+    {
+        "name": "Rembrandt Burger",
+        "address": "Richard-Sorge-Strasse 21, 10249 Berlin",
+        "lat": "52.520016",
+        "lng": "13.447797",
+        "type": "burger",
+        "phone": "030 89997296",
+        "email": "info@rembrandt-burger.de",
+        "url": "http://www.rembrandt-burger.de/",
+        "comment": "Mark: Sehr gutes Neulandfleisch, sehr gutes Burgerbrot wird getoastet. Die Saucen sind sehr interessant! <br/> Auf jeden Fall Pommes dazu bestellen!"
+    },
+    {
+        "name": "Burgers Berlin",
+        "address": "Goltzstrasse 33, 10781 Berlin",
+        "lat": "52.494274",
+        "lng": "13.353656",
+        "type": "burger"
+    },
+    {
+        "name": "Rebel Room",
+        "address": "Seestr. 93, 13347 Berlin",
+        "lat": "52.552147",
+        "lng": "13.354447",
+        "type": "burger",
+        "phone": "030 25011945"
+    },
+    {
+        "name": "Adana Grillhaus",
+        "address": "Manteuffelstr. 86, 10997 Berlin",
+        "lat": "52.499626",
+        "lng": "13.426880",
+        "type": "bbq",
+        "phone": "030 6127790"
+    },
+    {
+        "name": "Tadim Ocakkbasi",
+        "address": "Zossener Str. 46, 10961 Berlin",
+        "lat": "52.493019",
+        "lng": "13.395050",
+        "type": "bbq",
+        "phone": "030 50917710"
+    },
+    {
+        "name": "Harzer Schnitzelkoenig",
+        "address": "Wildemanner Strasse 9, 38685 Lautenthal",
+        "lat": "51.867432",
+        "lng": "10.285182",
+        "type": "bbq",
+        "phone": "05325 - 5887970",
+        "url": "http://www.harzer-schnitzelkoenig.de"
+    },
+    {
+        "name": "Bei Franz",
+        "address": "Donnerschweer Strasse 6, 26123 Oldenburg",
+        "lat": "53.145916",
+        "lng": "8.215674",
+        "type": "bbq",
+        "phone": "0441 882286"
+    },
+    {
+        "name": "Die Pfeffermuehle",
+        "address": "Ravenstrasse 2, 31061 Alfeld (Leine)",
+        "lat": "51.989136",
+        "lng": "9.821871",
+        "type": "bbq",
+        "phone": "05181 - 3087",
+        "url": "http://www.pfeffermuehle-alfeld.de/",
+        "comment": "Samstags mittags geschlossen - Dienstag Ruhetag.<br /><br />Premium Angus. Die kennen sogar die Sozialversicherungsnummern der Tiere, bevor sie liebevoll in die Pfanne gestreichelt werden. No. 1 Steakhaus im Grossraum Einbeck.<br /><br />- Kay"
+    },
+    {
+        "name": "Das Ding",
+        "address": "Woehlerstrasse 34, 30163 Hannover",
+        "lat": "52.397110",
+        "lng": "9.749226",
+        "type": "bbq",
+        "phone": "0511 - 39 29 99",
+        "email": "info@dasding-restaurant.de",
+        "url": "http://www.dasding-restaurant.de",
+        "comment": "Grossartig marinierte, super zarte Steaks direkt ueberm Eichenholzgrill zubereitet!<br /><br />- Cordula"
+    },
+    {
+        "name": "Pignut BBQ",
+        "address": "Arminiusstrasse 2, 10551 Berlin",
+        "lat": "52.527473",
+        "lng": "13.338527",
+        "type": "bbq",
+        "phone": "030 609853971",
+        "email": "info@pignut.de",
+        "url": "http://www.pignut.de",
+        "comment": "Try ribs & pulled pork!<br />Delicious.<br /><br />Stefan P."
+    },
+    {
+        "name": "Mogg & Melzer",
+        "address": "Auguststrasse 11, 10117 Berlin",
+        "lat": "52.526478",
+        "lng": "13.394578",
+        "type": "bbq",
+        "phone": "030 330060770",
+        "email": "info@moggandmelzer.com",
+        "url": "http://www.moggandmelzer.com",
+        "comment": "Best pastrami and bbq brisket."
+    },
+    {
+        "name": "Ruben & Carla",
+        "address": "Linienstrasse 136, 10115 Berlin",
+        "lat": "52.526314",
+        "lng": "13.388971",
+        "type": "bbq",
+        "phone": "030 27909683",
+        "email": "info@rubencarla.com",
+        "url": "http://www.rubencarla.com "
+    },
+    {
+        "name": "East London",
+        "address": "Mehringdamm 33, 10961 Berlin",
+        "lat": "52.493443",
+        "lng": "13.388508",
+        "type": "burger",
+        "phone": "030 69 53 32 05",
+        "email": "info@eastlondon.de",
+        "url": "http://www.eastlondon.de"
+    }
+]
